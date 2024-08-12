1c34460e5287fdcea05c8fe3a6c70d9a3bf4a25f
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2024-08-12 20:53:03 +0530

    Add files via upload

---
+++ swapping_of_numbers_without_third_variable.js
@@ -0,0 +1,7 @@
+var number1=10;
+var number2=20;
+console.log("Before swapping",number1,number2);
+number1=number1+number2;
+number2=number1-number2;
+number1=number1-number2;
+console.log("After swapping",number1,number2);
