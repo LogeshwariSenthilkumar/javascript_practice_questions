378790e86b3e802124f1b3badd3d4e9173ea8c7f
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2024-08-13 21:13:49 +0530

    Add files via upload

---
+++ simple_calculator.js
@@ -0,0 +1,24 @@
+function addition()
+{
+    console.log("Addition:",number1+number2);
+}
+function subtraction()
+{
+    console.log("Subtration:",number1-number2);
+}
+function multiplication()
+{
+    console.log("Multiplication:",number1*number2);
+}
+function division()
+{
+    console.log("Division:",number1/number2);
+}
+
+var number1=20;
+var number2=10;
+
+addition(number1,number2);
+subtraction(number1,number2);
+multiplication(number1,number2);
+division(number1,number2);
