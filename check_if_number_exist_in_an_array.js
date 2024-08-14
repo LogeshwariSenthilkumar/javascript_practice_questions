b4e01258db6fb33b1e310987e9b61ba387487ee5
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2024-08-14 21:55:59 +0530

    Add files via upload

---
+++ check_if_number_exist_in_an_array.js
@@ -0,0 +1,9 @@
+var my_array=[2,4,5,6,1,8,10,23];
+var target_number=10;
+for(var i=0;i<my_array.length;i++)
+{
+    if(my_array[i]==target_number)
+    {
+        console.log("Number exists in an array");
+    }
+}
