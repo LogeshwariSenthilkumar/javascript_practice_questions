0798d3b2d7fb325c69ab6f469c6da2eebb275f2f
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2024-08-15 20:45:37 +0530

    Add files via upload

---
+++ find_the_index_of_the_first_occurrence_of_a_specific_element_an_array.js
@@ -0,0 +1,9 @@
+var my_array=['f','r','u','i','t','s'];
+var target_element='u';
+for(i=0;i<my_array.length;i++)
+{
+    if(my_array[i]==target_element)
+    {
+        console.log("Specific element index is:",i);
+    }
+}
